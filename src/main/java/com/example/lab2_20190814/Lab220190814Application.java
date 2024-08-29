3e7c696b48c9d8df1edfbc6f1bf340882850f6bc
Al4cr4n <148933724+Al4cr4n@users.noreply.github.com>
2024-08-28 19:26:52 -0500

    first commit

---
+++ src/main/java/com/example/lab2_20190814/Lab220190814Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20190814;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab220190814Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab220190814Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab2_20190814/Lab220190814ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20190814;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab220190814ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
